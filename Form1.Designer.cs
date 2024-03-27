4c800faf28c3985339923759828d21648fb11191
Shannon Tran <Shannon Tran@DESKTOP-KMF4OEF>
2024-03-27 18:52:27 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ClickLabel1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ClickLabel2 = new System.Windows.Forms.Label();
+            this.ClickLabel3 = new System.Windows.Forms.Label();
+            this.ClickLabel4 = new System.Windows.Forms.Label();
+            this.ClickLabel5 = new System.Windows.Forms.Label();
+            this.ClickLabel6 = new System.Windows.Forms.Label();
+            this.ClickLabel7 = new System.Windows.Forms.Label();
+            this.ClickLabel8 = new System.Windows.Forms.Label();
+            this.ClickLabel9 = new System.Windows.Forms.Label();
+            this.tictactoeBox1 = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClickLabel1
+            // 
+            this.ClickLabel1.Location = new System.Drawing.Point(227, 290);
+            this.ClickLabel1.Name = "ClickLabel1";
+            this.ClickLabel1.Size = new System.Drawing.Size(129, 41);
+            this.ClickLabel1.TabIndex = 0;
+            this.ClickLabel1.Text = "Click Here";
+            this.ClickLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ClickLabel1.Click += new System.EventHandler(this.ClickLabel1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TicTacToe.Properties.Resources.ttt_board2;
+            this.pictureBox2.Location = new System.Drawing.Point(170, 197);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(643, 446);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TicTacToe.Properties.Resources.tictactoee;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(119, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(714, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ClickLabel2
+            // 
+            this.ClickLabel2.Location = new System.Drawing.Point(438, 290);
+            this.ClickLabel2.Name = "ClickLabel2";
+            this.ClickLabel2.Size = new System.Drawing.Size(120, 41);
+            this.ClickLabel2.TabIndex = 3;
+            this.ClickLabel2.Text = "Click Here";
+            this.ClickLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ClickLabel2.Click += new System.EventHandler(this.ClickLabel2_Click);
+            // 
+            // ClickLabel3
+            // 
+            this.ClickLabel3.Location = new System.Drawing.Point(638, 290);
+            this.ClickLabel3.Name = "ClickLabel3";
+            this.ClickLabel3.Size = new System.Drawing.Size(129, 41);
+            this.ClickLabel3.TabIndex = 4;
+            this.ClickLabel3.Text = "Click Here";
+            this.ClickLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel4
+            // 
+            this.ClickLabel4.Location = new System.Drawing.Point(227, 428);
+            this.ClickLabel4.Name = "ClickLabel4";
+            this.ClickLabel4.Size = new System.Drawing.Size(129, 44);
+            this.ClickLabel4.TabIndex = 5;
+            this.ClickLabel4.Text = "Click Here";
+            this.ClickLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel5
+            // 
+            this.ClickLabel5.Location = new System.Drawing.Point(429, 428);
+            this.ClickLabel5.Name = "ClickLabel5";
+            this.ClickLabel5.Size = new System.Drawing.Size(129, 44);
+            this.ClickLabel5.TabIndex = 6;
+            this.ClickLabel5.Text = "Click Here";
+            this.ClickLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel6
+            // 
+            this.ClickLabel6.Location = new System.Drawing.Point(638, 428);
+            this.ClickLabel6.Name = "ClickLabel6";
+            this.ClickLabel6.Size = new System.Drawing.Size(129, 44);
+            this.ClickLabel6.TabIndex = 7;
+            this.ClickLabel6.Text = "Click Here";
+            this.ClickLabel6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel7
+            // 
+            this.ClickLabel7.Location = new System.Drawing.Point(230, 568);
+            this.ClickLabel7.Name = "ClickLabel7";
+            this.ClickLabel7.Size = new System.Drawing.Size(126, 48);
+            this.ClickLabel7.TabIndex = 8;
+            this.ClickLabel7.Text = "Click Here";
+            this.ClickLabel7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel8
+            // 
+            this.ClickLabel8.Location = new System.Drawing.Point(432, 568);
+            this.ClickLabel8.Name = "ClickLabel8";
+            this.ClickLabel8.Size = new System.Drawing.Size(126, 48);
+            this.ClickLabel8.TabIndex = 9;
+            this.ClickLabel8.Text = "Click Here";
+            this.ClickLabel8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickLabel9
+            // 
+            this.ClickLabel9.Location = new System.Drawing.Point(641, 568);
+            this.ClickLabel9.Name = "ClickLabel9";
+            this.ClickLabel9.Size = new System.Drawing.Size(126, 48);
+            this.ClickLabel9.TabIndex = 10;
+            this.ClickLabel9.Text = "Click Here";
+            this.ClickLabel9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tictactoeBox1
+            // 
+            this.tictactoeBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tictactoeBox1.Location = new System.Drawing.Point(240, 222);
+            this.tictactoeBox1.Name = "tictactoeBox1";
+            this.tictactoeBox1.Size = new System.Drawing.Size(95, 68);
+            this.tictactoeBox1.TabIndex = 11;
+            this.tictactoeBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(304, 683);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(126, 48);
+            this.ResetButton.TabIndex = 12;
+            this.ResetButton.Text = "Reset Game";
+            this.ResetButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(465, 683);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(126, 48);
+            this.ExitButton.TabIndex = 13;
+            this.ExitButton.Text = "Exit Game";
+            this.ExitButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 768);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.tictactoeBox1);
+            this.Controls.Add(this.ClickLabel9);
+            this.Controls.Add(this.ClickLabel8);
+            this.Controls.Add(this.ClickLabel7);
+            this.Controls.Add(this.ClickLabel6);
+            this.Controls.Add(this.ClickLabel5);
+            this.Controls.Add(this.ClickLabel4);
+            this.Controls.Add(this.ClickLabel3);
+            this.Controls.Add(this.ClickLabel2);
+            this.Controls.Add(this.ClickLabel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ClickLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label ClickLabel2;
+        private System.Windows.Forms.Label ClickLabel3;
+        private System.Windows.Forms.Label ClickLabel4;
+        private System.Windows.Forms.Label ClickLabel5;
+        private System.Windows.Forms.Label ClickLabel6;
+        private System.Windows.Forms.Label ClickLabel7;
+        private System.Windows.Forms.Label ClickLabel8;
+        private System.Windows.Forms.Label ClickLabel9;
+        private System.Windows.Forms.Label tictactoeBox1;
+        private System.Windows.Forms.Label ResetButton;
+        private System.Windows.Forms.Label ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+
+        //To change the player
+        int playerChange = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickLabel1_Click(object sender, EventArgs e)
+        {
+            // Check whose turn it is and update the box accordingly
+            if (playerChange == 0)
+            {
+                tictactoeBox1.Text = "X";
+                playerChange++;
+            }
+            else if (playerChange == 1)
+            {
+                tictactoeBox1.Text = "O";
+                playerChange--;
+            }
+        }
+
+        private void ClickLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
